bc7299feeea679696dc0e1426d35fbba594e6e61
tomjeastar <borostamas435@gmail.com>
2022-12-02 12:19:01 +0100

    webshop

--- script.js
+++ script.js
@@ -1,73 +1,28 @@
-/*
-Create (új sor, új objektum)
-Read (táblázat lista)
-Update (sor (objektum) módoisítás)
-Delete (sor tötlés)
+let products = [];
+let url = "http://localhost:3000/products";
+let mode = "read";
+let currentID = null;
+let order = false;
+let cart = [];
 
-CRUD műveletek
-*/
-
-//state
-
-state = {
-    //Adatstruktúra
-    products: [
-        {
-            id: idGen(),
-            name: "Áru 1",
-            price: 1500,
-            quantity: 97,
-            isInStock: true
-        },
-        {
-            id: idGen(),
-            name: "Áru 2",
-            price: 2500,
-            quantity: 15,
-            isInStock: true
-        },
-        {
-            id: idGen(),
-            name: "Áru 3",
-            price: 3500,
-            quantity: 25,
-            isInStock: false
-        },
-        {
-            id: idGen(),
-            name: "Áru 4",
-            price: 4500,
-            quantity: 10,
-            isInStock: true
-        }
-    ],
-
-    cart: [],
-
-    event: "read", //milyen állapotban van: read, delete, update, create
-    currentId: null //Update esetén itt tároljuk a módosítandó product id-jét
-}
-
-//#region Segéd függvények
-//Űrlap megjelenítése
-function formView(){
+function formView() {
     document.getElementById("form").classList.remove("d-none")
 }
 
 //űrlap elrejtése
-function formHide(){
+function formHide() {
     document.getElementById("form").classList.add("d-none")
 }
 
 //Id generátor
-function idGen(){
+function idGen() {
     return Date.now().toString(36) + Math.random().toString(36).substr(2);
 }
 
 //id alapján megkeresi az index-et: id -> index
-function searchIndex(id){
-    for (let index = 0; index < state.products.length; index++) {
-        if (id === state.products[index].id) {
+function searchIndex(id) {
+    for (let index = 0; index < products.length; index++) {
+        if (id === products[index].id) {
             return index;
         }
     }
@@ -75,95 +30,126 @@ function searchIndex(id){
 //#endregion 
 
 //Mégse gomb működtetése
-document.getElementById("cancel-product").onclick=function(){
-    state.event = "read";
+document.getElementById("cancel-product").onclick = function() {
+    mode = "read";
     formHide();
 };
 
 //Create: Új áru gomb
-document.getElementById("new-product").onclick = function(id){
-    state.event = "create";
-    //látszódjon az Új áru cím
-
-    document.getElementById("title-new").classList.remove("d-none");
-    document.getElementById("title-update").classList.add("d-none");
+document.getElementById("new-product").onclick = function(id) {
+    document.getElementById("formHead").innerHTML = "Új áru bevitele"
+    mode = "create";
     formView();
 };
 
 //Save: Mentés gomb
-document.getElementById("save-product").onclick = function(event){
+document.getElementById("save-product").onclick = function(event) {
     event.preventDefault();
+    event = "create";
 
-    //Hozzájutás az adatokhoz
-    let name = document.getElementById("name").value;
-    let price = +document.getElementById("price").value;
-    let isInStock = document.getElementById("isInStock").checked;
+    if (mode == "create") {
+        let product = {
+            id: idGen(),
+            name: document.getElementById("name").value,
+            quantity: document.getElementById("quantity").value,
+            price: document.getElementById("price").value,
+            type: document.getElementById("type").value
+        }
+        fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(product)
+            })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                renderProducts();
+            });
 
-    //validálás
+    } else if (mode == "update") {
+        let product = {
+            id: currentID,
+            name: document.getElementById("name").value,
+            quantity: document.getElementById("quantity").value,
+            price: document.getElementById("price").value,
+            type: document.getElementById("type").value
+        };
+
+        let urlUpdate = `${url}/${currentID}`
+        fetch(urlUpdate, {
+                method: 'PUT',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(product)
+            })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                renderProducts();
+            });
+    }
+
     let errorList = [];
-    if (! (name)){
+    if (!(products.name)) {
         console.log("namehiba");
         document.getElementById("name-label").classList.add("text-danger");
         errorList.push("Name hiba");
-    }else{
+    } else {
         document.getElementById("name-label").classList.remove("text-danger");
     }
-    if (! (price)){
+    if (!(price)) {
         console.log("namehiba");
         document.getElementById("price-label").classList.add("text-danger");
         errorList.push("Price hiba");
-    }else{
+    } else {
         document.getElementById("price-label").classList.remove("text-danger");
     }
+    if (!(type)) {
+        console.log("typehiba");
+        document.getElementById("type-label").classList.add("text-danger");
+        errorList.push("Type hiba");
+    } else {
+        document.getElementById("type-label").classList.remove("text-danger");
+    }
+    if (!(quantity)) {
+        console.log("quantityHiba");
+        document.getElementById("quantity-label").classList.add("text-danger");
+        errorList.push("Quantity hiba");
+    } else {
+        document.getElementById("quantity-label").classList.remove("text-danger");
+    }
 
-    if (errorList.length >0) {
+    if (errorList.length > 0) {
         return;
     }
 
-   //alapban generálunk
-    let id = idGen();
-    if(state.event === "update") {
-        //update: az kéne, amire kattintottunk
-        id = state.currentId;
-    }
-  
 
-    let product = {
-        id: id,
-        name: name,
-        price: price,
-        isInStock: isInStock
-    }
-
-    if (state.event == "create" ) {
-        state.products.push(product);
-    }
-   else if (state.event = "update") {
-        let index = searchIndex(id);
-        state.products[index] = product;
-    }
-    
-    renderProducts();
     formHide()
 
     //mezők ürítése
     document.getElementById("name").value = null;
     document.getElementById("price").value = null;
+    document.getElementById("quantity").value = null;
+    document.getElementById("type").value = null;
 }
 
 //Kosár megmutatása
-function cartRender(){
+function cartRender() {
     //kosár ablak megjelenítése
     cardBoxView();
 
     //lista előállítása
     let cardHtml = "";
     let total = 0
-    //végigmegyünk a kosáron (state.cart)
-    for (const product of state.cart) {
+    for (const product of cart) {
         cardHtml += `
         <li class="list-group-item">
-            ${product.name}, ${product.price} Ft/db, ${product.quantity}db ár: ${product.price*product.quantity} Ft
+            ${product.name}, ${product.type}, ${product.price} Ft/db, ${product.quantity}db ár: ${product.price*product.quantity} Ft
             <button
               type="button"
               class="btn btn-danger btn-sm"
@@ -173,7 +159,7 @@ function cartRender(){
             </button>
         </li>
         `;
-        total += product.price*product.quantity;
+        total += product.price * product.quantity;
     }
 
     //lista berkása az ul-be
@@ -183,39 +169,39 @@ function cartRender(){
 }
 
 //kosár áru mennyiség kiszámolása, és beírása
-function renderCartCount(){
+function renderCartCount() {
     //mennyi áru van a kosárban?
-    let count = state.cart.length;
+    let count = cart.length;
     //Írd ki ezt az értéket a "cart-count"-ba
-    document.getElementById("cart-count").innerHTML=count;
+    document.getElementById("cart-count").innerHTML = count;
 }
 
 //Törlés a kosárból
 //issue: Törlés a kosárból
-function deleteFromCart(id){
+function deleteFromCart(id) {
     //megkeressük a cart-ban az idexet ami az id-hez tartozik
     let index = seachIndexByIdInCart(id);
     //kiszedjük a kosárból az index-hez tartozó árut
-    
+
     //darabszám korrekció
     //1. megkesem a darabszámot
-    let quantity = state.cart[index].quantity;
+    let quantity = cart[index].quantity;
     //2. megkeresem a ratkárban a kitörölt árut
     let indexPducts = searchIndex(id);
     //3. korrigálom a darbszámát
-    state.products[indexPducts].quantity += quantity;
-    
-    state.cart.splice(index,1);
+    products[indexPducts].quantity += quantity;
+
+    cart.splice(index, 1);
     //render: kosár, kártyák
     cartRender();
     renderProducts();
 }
 
 //megkeressük a cart-ban az idexet ami az id-hez tartozik
-function seachIndexByIdInCart(id){
+function seachIndexByIdInCart(id) {
     let indexReturn = -1;
-    for (let index = 0; index < state.cart.length; index++) {
-        if (state.cart[index].id == id) {
+    for (let index = 0; index < cart.length; index++) {
+        if (cart[index].id == id) {
             indexReturn = index;
             break;
         }
@@ -226,184 +212,191 @@ function seachIndexByIdInCart(id){
 
 //A fizetés folymata
 //issue: ki kell doglozni a fizetés folymatát
-function payRender(){
+function payRender() {
     console.log("payRender()");
     cartBoxHide();
 }
 
 //Tovább vásárolok
-function continueBy(){
+function continueBy() {
     console.log("continueBy()");
     cartBoxHide();
 }
 
 //Kosár eltüntetése
-function cartBoxHide(){
+function cartBoxHide() {
     document.getElementById("cart-box").classList.add("d-none");
 }
 
 //kosár megjelenítése
-function cardBoxView(){
+function cardBoxView() {
     document.getElementById("cart-box").classList.remove("d-none");
 
 }
 
 
-    
 
-//Read: product lista
-function renderProducts(){
-    console.log(state.products);
-    state.event = "read";
+
+function renderProducts() {
+
+    mode = "read";
+    fetch(url)
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            console.log("Adatok a szervertől", data)
+            products = data
+            console.log("products", products);
+            viewProducts(data);
+        })
+        .catch((error) => {
+            console.log(error)
+            error = "Szerver hiba"
+            document.getElementById("table").innerHTML = `<h2>${error}</h2>`;
+
+        });
+
+}
+
+function viewProducts(products) {
+
+
     let prodctsHtml = "";
-    
-    state.products.forEach(product => {
+    products.forEach(product => {
         prodctsHtml += `
-        <div class="col">
-            <div class="card ${product.quantity > 0 ? "" : "bg-warning"}">
-                <div class="card-body">
-                    <h5 class="card-title">${product.name}</h5>
-                    <p class="card-text">Termék ár: ${product.price} Ft</p>
-                    <p class="card-text">Raktáron: ${product.quantity} db</p>
-                </div>
-
-                <div class="d-flex flex-row m-2">
-
-                    <!-- Törlés -->
-                    <button type="button" 
-                        class="btn btn-danger btn-sm"
-                        onclick="deleteProduct('${product.id}')"
-                    >
-                        Törlés
-                    </button>
-
-                    <!-- Módosítás -->
-                    <button type="button" 
-                        class="btn btn-success btn-sm ms-2"
-                        onclick="updateProduct('${product.id}')"
-                    >
-                        Módosít
-                    </button>
-                </div>
-
-                <div class="d-flex flex-row m-2">
-                    <!-- Kosárba rakás -->
-                    <button type="button" 
-                        class="btn btn-outline-success col-4"
-                        onclick="intoCart('${product.id}')"
-                    >
-                        <i class="bi bi-cart-plus"></i>
-                    </button>
-                    
-                    <!-- Mennyit rakok a kosárba -->
-                    <input
-                        type="number"
-                        class="form-control ms-2"
-                        id="${product.id}"
-                        value="1"
-                        min="1"
-                        max="${product.quantity}"
-                        onchange="quantityInputCheck('${product.id}')"
-                    />
-                </div>
+    <div class="col">
+        <div class="card ${product.quantity > 0 ? "" : "bg-warning"}">
+            <div class="card-body">
+                <h5 class="card-title">${product.name}</h5>
+                <p class="card-text">Termék ár: ${product.price} Ft</p>
+                <p class="card-text">Raktáron: ${product.quantity} db</p>
+                <p class="card-text">Típus: ${product.type}</p>
             </div>
-        </div>`;
-        
+
+            <div class="d-flex flex-row m-2">
+
+                <!-- Törlés -->
+                <button type="button" 
+                    class="btn btn-danger btn-sm"
+                    onclick="deleteProduct('${product.id}')"
+                >
+                    Törlés
+                </button>
+
+                <!-- Módosítás -->
+                <button type="button" 
+                    class="btn btn-success btn-sm ms-2"
+                    onclick="updateProduct('${product.id}')"
+                >
+                    Módosít
+                </button>
+            </div>
+
+            <div class="d-flex flex-row m-2">
+                <!-- Kosárba rakás -->
+                <button type="button" 
+                    class="btn btn-outline-success col-4"
+                    onclick="intoCart('${product.id}')"
+                >
+                    <i class="bi bi-cart-plus"></i>
+                </button>
+                
+                <!-- Mennyit rakok a kosárba -->
+                <input
+                    type="number"
+                    class="form-control ms-2"
+                    id="${product.id}"
+                    value="1"
+                    min="1"
+                    max="${product.quantity}"
+                    onchange="quantityInputCheck('${product.id}')"
+                />
+            </div>
+        </div>
+    </div>`;
+
     });
     document.getElementById("product-list").innerHTML = prodctsHtml;
 }
 
-function quantityInputCheck(id){
+function quantityInputCheck(id) {
     //kiszedjük mi van beleírva
     let quantity = +document.getElementById(id).value;
     console.log("check", id, quantity);
 
     //kiszedjük az id alpján, hogy a raktrban mennyi van belőle
     let index = searchIndex(id);
-    let quantityProduct = state.products[index].quantity;
+    let quantityProduct = products[index].quantity;
     //vizsgálódás, ha többet, vagy negatívot írtunk, akkor korrigálunk
     if (quantity < 0) {
         document.getElementById(id).value = 1;
-    } else if(quantity > quantityProduct) {
+    } else if (quantity > quantityProduct) {
         document.getElementById(id).value = quantityProduct;
     }
 }
 
-//Kosár
-//issue: nem kell az isInsStock: bevitel, és egyéb helyeken
-function intoCart(id){
+
+function intoCart(id) {
     //Derítsük ki az indexet
     let index = searchIndex(id);
-    
+
     let quantity = +document.getElementById(`${id}`).value
 
     //Mennyiség korrektció:
     //le kell vonni az eredeti mennyiségből
-    state.products[index].quantity = state.products[index].quantity - quantity;
+    products[index].quantity = products[index].quantity - quantity;
 
-    // let product = {
-    //     id: state.products[index].id,
-    //     name: state.products[index].name,
-    //     price: state.products[index].price,
-    //     quantity: quantity,
-    //     isInStock: state.products[index].isInStock
-    // }
-    let product = {...state.products[index]}
+
+    let product = {...products[index] }
     product.quantity = quantity;
 
-    // let product = state.products[index];
 
     //van-a kosárban id-jű áru???
     let indexCart = seachIndexByIdInCart(id);
     if (indexCart === -1) {
         //még nincs ilyen áru a kosárban
         //push a kosárba
-        state.cart.push(product);
+        cart.push(product);
     } else {
-        state.cart[indexCart].quantity += quantity;
+        cart[indexCart].quantity += quantity;
     }
-    //a kosárba ezzel amennyiséggel kell berakni
 
-
-    //újrarendereljük a termékeket
     renderProducts();
     renderCartCount()
 
     //logojuk a kosarat
-    console.log(state.cart);
+    console.log(cart);
 
 }
 
-
-
-//Update: Módosít gomb függvénye
-function updateProduct(id){
-    state.event = "update"
-    state.currentId = id;
-    //kerüljenek bele az űrlapba a kártya datai
-    let index = searchIndex(id);
-    //beolvassuk az űrlapba
-    let name = state.products[index].name
-    let price = state.products[index].price
-    let isInStock = state.products[index].isInStock
-    document.getElementById("name").value = name;
-    document.getElementById("price").value = price;
-    document.getElementById("isInStock").checked = isInStock;
-
-    document.getElementById("title-update").classList.remove("d-none");
-    document.getElementById("title-new").classList.add("d-none");
-
-    formView();
-    console.log(id);
+function updateProduct(id) {
+    document.getElementById("formHead").innerHTML = "Áru módosítása"
+    mode = "update";
+    let urlUpdate = `${url}/${id}`
+    fetch(urlUpdate)
+        .then((response) => response.json())
+        .then((data) => {
+            currentID = data.id;
+            document.getElementById("name").value = data.name;
+            document.getElementById("quantity").value = data.quantity;
+            document.getElementById("price").value = data.price;
+            document.getElementById("type").value = data.type;
+        })
+    formView()
 }
 
-//Delete: Töröl gomb függvénye
-function deleteProduct(id){
-    state.event = "delete";
-    let index = searchIndex(id)
-    state.products.splice(index,1);
-    renderProducts()
+function deleteProduct(id) {
+    let urlDelete = `${url}/${id}`
+    fetch(urlDelete, {
+            method: 'DELETE'
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            renderTable();
+        })
+        .catch((error) => console.log(error))
 }
 
-//Amikor betöltődött az oldal, elindul a: renderProducts függvény
 window.onload = renderProducts;
